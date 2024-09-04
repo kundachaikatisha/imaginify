4675c967098ae7fa37551cd27a9e855956d33a6c
Kunda Chaikatisha <kuchaika@microsoft.com>
2024-09-04 14:44:35 +0200

    with changes

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,12 @@
+import Header from '@/components/shared/Header'
 import React from 'react'
 
 const AddTransformationTypePage = () => {
   return (
-    <div>
-      AddTransformationTypePage
-    </div>
+    <Header
+      title = "Transformation Title"
+      subtitle = "Transformation SubTitle"
+     />
   )
 }
 
