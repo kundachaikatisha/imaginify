6da6468f6d4b78f0ce9c6ca1f3f5b36c38a6c833
Kunda Chaikatisha <kuchaika@microsoft.com>
2024-09-04 15:35:23 +0200

    one more

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import TransformationForm from './TransformationForm'
 
 const Header = ({ title, subtitle}: {title: string, subtitle?: string}) => {
   return (
     <>
-      <h2 className="h2-bold text-dark-600">{title}</h2>
-      {subtitle && <p className="p-16-regular mt-4"> {subtitle} </p>}
+      <>
+        <h2 className="h2-bold text-dark-600">{title}</h2>
+        {subtitle && <p className="p-16-regular mt-4"> {subtitle} </p>}
+      </>
+
+      <TransformationForm/>
     </>
+
   )
 }
 
